bc594abf891c2dd6bc41952c7afb6cffd39143b6
Nikolay Cherepov <aeconick@gmail.com>
2023-09-14 20:59:43 +0300

    Add get and post controller

--- server/index.js
+++ server/index.js
@@ -12,8 +12,33 @@ const db = mysql.createConnection({
   database: "test",
 });
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json("hello this is the backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+  db.query(q, [values], (err, data) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 app.listen(8800, () => {

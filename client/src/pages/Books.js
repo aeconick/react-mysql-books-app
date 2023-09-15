e10c1a8a563e84c5b2291ad170d5edead6460619
Nikolay Cherepov <aeconick@gmail.com>
2023-09-15 19:29:15 +0300

    Add delete functionality

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -17,6 +17,15 @@ const Books = () => {
     fetchBooks();
   }, []);
 
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete("http://localhost:8800/books/" + id);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <h1>Nikolay's Book Shop</h1>
@@ -27,7 +36,9 @@ const Books = () => {
             <h2>{book.title}</h2>
             <p>{book.description}</p>
             <span>{book.price}</span>
-            <button className="delete">Delete</button>
+            <button className="delete" onClick={() => handleDelete(book.id)}>
+              Delete
+            </button>
             <button className="update">Update</button>
           </div>
         ))}
